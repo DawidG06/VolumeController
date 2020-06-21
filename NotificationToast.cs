34c7b2db5060b54a337b2e02ede5cb47d0a6db83
DawidG <dawid.gretka@gmail.com>
2020-06-21 18:58:49 +0200

    init

---
+++ NotificationToast.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeController
+{
+    class NotificationToast
+    {
+
+        private static NotificationToast _instance = new NotificationToast();
+        public static NotificationToast getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new NotificationToast();
+            }
+
+            return _instance;
+        }
+
+        private readonly NotifyIcon _notifyIcon;
+        private NotificationToast()
+        {
+            _notifyIcon = new NotifyIcon();
+            // Extracts your app's icon and uses it as notify icon
+            _notifyIcon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            // Hides the icon when the notification is closed
+            _notifyIcon.BalloonTipClosed += (s, e) => _notifyIcon.Visible = false;
+        }
+
+
+        public void Show(string title, string message, ToolTipIcon icon = ToolTipIcon.None, int timeout = 3000)
+        {
+            _notifyIcon.Visible = true;
+            // Shows a notification with specified message and title
+            _notifyIcon.ShowBalloonTip(timeout, title, message, icon);
+            
+        }
+
+        public void uv(string tt)
+        {
+            _notifyIcon.BalloonTipText = tt;
+            _notifyIcon.Text = tt;
+        }
+        
+
+    }
+}
---
+++ VolumeManager.cs
@@ -0,0 +1,19 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeController
+{
+    class VolumeManager
+    {
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+        public void up()
+        {
+            defaultPlaybackDevice.ToggleMute();
+        }
+    }
+}
